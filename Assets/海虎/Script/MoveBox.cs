84ed5e813d3ea10ef9c9337941ef9d9731f3e0ab
JunLangBai <1279477694@qq.com>
2024-11-26 22:47:29 +0800

    草泥马的BUG草泥马

--- Assets/海虎/Script/MoveBox.cs
+++ Assets/海虎/Script/MoveBox.cs
@@ -1,24 +1,41 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class MoveBox : MonoBehaviour
 {
     public LayerMask decectlayer;
     public int boxID;
-    public event Action<int, Vector2> OnBoxMoved; // 定义事件，传递ID和移动方向
+    // 重点依据这个变量来进行同步分组
+    public int mapCorrespondenceID;
+
+    private void Awake()
+    {
+        // 调用BoxManager的注册方法，传入mapCorrespondenceID来进行分组注册
+        BoxManager.RegisterBox(mapCorrespondenceID, this.gameObject);
+    }
 
     public bool MoveToBox(Vector2 dir)
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir,1f,decectlayer);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, 1f, decectlayer);
         if (!hit && boxID == GameControl.Instance.moveBoxID)
         {
+            // 获取BoxManager中具有相同mapCorrespondenceID的所有物体列表
+            List<GameObject> sameMapCorrespondenceIDObjects = BoxManager.GetBoxesWithID(mapCorrespondenceID);
+            foreach (GameObject obj in sameMapCorrespondenceIDObjects)
+            {
+                // 获取对应物体上挂载的MoveBox组件
+                MoveBox otherMoveBox = obj.GetComponent<MoveBox>();
+                if (otherMoveBox!= null && otherMoveBox.boxID == this.boxID)
+                {
+                    // 假设添加一个移动速度变量 speed，这里乘以Time.deltaTime来保证帧率无关性
+                    float speed = 5f;
+                    otherMoveBox.transform.Translate(dir);
+                }
+            }
             //transform.Translate(dir);
-            OnBoxMoved?.Invoke(boxID, dir); // 移动后触发事件，传递ID和方向
             return true;
         }
-        
-        return false;    
+
+        return false;
     }
 }
--- Assets/海虎/Script/PlayerMove.cs
+++ Assets/海虎/Script/PlayerMove.cs
@@ -1,82 +1,86 @@
-    using System.Collections;
-    using System.Collections.Generic;
-    using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-    public class PlayerMove : MonoBehaviour
+public class PlayerMove : MonoBehaviour
+{
+    private Vector2 movedir;
+    public LayerMask layerMask;
+    private GameControl gameControl; // 新增，用于存储GameControl单例实例
+
+    private void Start()
     {
-        private Vector2 movedir;
-        public LayerMask layerMask;
-        private GameControl gameControl; // 新增，用于存储GameControl单例实例
-
-        private void Start()
+        gameControl = GameControl.Instance; // 获取GameControl单例实例
+        if (gameControl == null)
         {
-            gameControl = GameControl.Instance; // 获取GameControl单例实例
-            if (gameControl == null)
-            {
-                Debug.LogError("GameControl instance not found!");
-            }
-        }
-
-
-        private void Update()
-        {
-            MovePlayer();
-        }
-
-        public void MovePlayer()
-        {
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                movedir = Vector2.up;
-            }
-            else if (Input.GetKeyDown(KeyCode.S))
-            {
-                movedir = Vector2.down;
-            }
-            else if (Input.GetKeyDown(KeyCode.A))
-            {
-                movedir = Vector2.left;
-            }
-            else if (Input.GetKeyDown(KeyCode.D))
-            {
-                movedir = Vector2.right;
-            }
-            if (movedir != Vector2.zero)
-            {
-                if (CanMove(movedir))
-                {
-                    MovePlayer(movedir);
-                }
-            }
-        
-            movedir = Vector2.zero;
-        }
-
-        public bool CanMove(Vector2 movedir)
-        {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, movedir, 1f, layerMask);
-            if (!hit)
-            {
-                return true;
-            }
-            else
-            {
-                MoveBox moveBox = hit.collider.gameObject.GetComponent<MoveBox>();
-                if (moveBox!= null)
-                {
-                    // 新增代码，检查箱子ID是否符合可移动条件
-                    if (moveBox.boxID == gameControl.moveBoxID)
-                    {
-                        return moveBox.MoveToBox(movedir);
-                    }
-                    return false; // 如果ID不符合，直接返回false，表示不可移动
-                }
-            }
-            return false;
-        }
-
-        public void MovePlayer(Vector2 movedir)
-        {
-            transform.Translate(movedir);
+            Debug.LogError("GameControl instance not found!");
         }
     }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movedir = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            movedir = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            movedir = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            movedir = Vector2.right;
+        }
+        if (movedir!= Vector2.zero)
+        {
+            if (CanMove(movedir))
+            {
+                MovePlayer(movedir);
+            }
+            // 获取玩家控制的方块（这里假设玩家控制的方块上挂载了MoveBox脚本，可以通过合适方式获取，比如直接关联等）
+            MoveBox playerMoveBox = GetComponent<MoveBox>();
+            if (playerMoveBox!= null)
+            {
+                // 调用MoveToBox方法，现在是基于mapCorrespondenceID来同步其他方块移动
+                playerMoveBox.MoveToBox(movedir);
+            }
+        }
+        movedir = Vector2.zero;
+    }
+
+    public bool CanMove(Vector2 movedir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, movedir, 1f, layerMask);
+        if (!hit)
+        {
+            return true;
+        }
+        else
+        {
+            MoveBox moveBox = hit.collider.gameObject.GetComponent<MoveBox>();
+            if (moveBox!= null)
+            {
+                if (moveBox.boxID == gameControl.moveBoxID)
+                {
+                    return moveBox.MoveToBox(movedir);
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+
+    public void MovePlayer(Vector2 movedir)
+    {
+        transform.Translate(movedir);
+    }
+}
