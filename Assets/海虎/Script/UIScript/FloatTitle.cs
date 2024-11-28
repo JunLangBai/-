18838bb14a7c2e04c72e94598a5226b90d7add0f
cab1120 <3029067093@qq.com>
2024-11-28 19:53:11 +0800

    6

--- Assets/海虎/Script/UIScript/FloatTitle.cs
+++ Assets/海虎/Script/UIScript/FloatTitle.cs
@@ -16,18 +16,33 @@ public class FloatTitle : MonoBehaviour
 
     void Start()
     {
-        // 记录小球的初始Y轴位置
-        startY = transform.position.y;
-        startX = transform.position.x;
+        
     }
 
     void Update()
     {
+        FloatMove(title);
+        FloatMove(O_o);
+    }
+
+    public void FloatMove(GameObject target)
+    {
+        // 记录小球的初始Y轴位置
+        startY = target.transform.position.y;
+        startX = target.transform.position.x;
+        
         // 使用正弦函数计算浮动的偏移量
         float newY = startY + Mathf.Sin(Time.time * floatSpeed) * floatAmplitude;
         float newX = startX + Mathf.Sin(Time.time * floatSpeed) * floatAmplitude;
 
         // 更新小球的Y轴位置，使其上下浮动
-        transform.position = new Vector3(newX, newY, transform.position.z);
+        if (target == title)
+        {
+            target.transform.position = new Vector3(newX, newY, target.transform.position.z);
+        }
+        else if (target == O_o)
+        {
+            target.transform.position = new Vector3(target.transform.position.x, newY, target.transform.position.z);
+        }
     }
 }
